3baf359d6f197c102a11e12298de7d49c57f0321
Manav Singla <manavsinghla123@gmail.com>
2025-11-03 12:54:30 +0530

    03/11/25

--- httpserver.js
+++ httpserver.js
@@ -7,3 +7,8 @@ const server=http.createServer((req, res) => {
 server.listen(3000, () => {
     console.log('Server is running at http://localhost:3000/');
 });
+
+const server2=http.createServer((req,res)=>{
+    console.log('Request URL:', req.url);
+    console.log('Request method:', req.method);
+});
